0b1636c283635612c939ffc92e643f0b1c414ff9
unknown <nawazchowdhury92@gmail.com>
2022-08-06 00:05:46 -0400

    Version 7

--- detail.php
+++ detail.php
@@ -19,16 +19,84 @@
 
 				  	<div class="col-md-8">
 
+
+
 				  <div class="card-body">
-				  	<h2> Product Title</h2>
-				    <h5 class="card-title">Samsung Mobile</h5>
-				    <p><b>Price $290 CAD</b></p>
-				    <p class="card-text">Samsung s21 latest mobile with</p>
 
-				    <h3>Detail:</h3>
-				    <p>A product detail page (PDP) is a web page on an eCommerce site that presents the description of a specific product in view. The details displayed often include size, color, price, shipping information, reviews, and other relevant information customers may want to know before making a purchase</p>
 
-				    <a href="detail.html" class="btn btn-primary">Place Order</a>
+
+			 <?php 
+
+                                            include('inc/db_connect.php'); 
+
+                                            $sql = "SELECT * FROM tbl_product WHERE p_id='".$_GET['id']."'";
+                                            $result = $conn->query($sql);
+
+                                            if ($result->num_rows > 0) {
+                                              // output data of each row
+                                              while($row = $result->fetch_assoc()) {  ?> 
+
+
+                                              	 	<h2> <?=$row['p_name']?></h2>
+												     
+												    <p><b>Price <?=$row['p_price']?> CAD</b></p>
+											 
+												    <h3>Detail:</h3>
+												    <p><?=$row['p_description']?></p>
+
+												      <?php if(isset($_SESSION['user_type'])){ ?>
+
+												    <a href="add_order.php?id=<?=$row['p_id']?>" class="btn btn-primary">Place Order</a>
+
+													<?php }else{  ?>
+													<!-- Button trigger modal -->
+
+													<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+													Place Order
+													</button>
+
+													<?php }  ?>
+  
+												
+
+                                    
+
+                                                
+                                             <?php 
+                                                 }
+                                                } else {
+                                                 // echo "0 results";
+                                                }
+                                            $conn->close();
+
+
+                                        ?>
+
+                                        <!-- Modal -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Login Required</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Please Login To Your Account To Place an Order
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <a href="login.php"><button type="button" class="btn btn-primary">Login</button></a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+			
+				 
 				  </div>
 
 				  </div>
@@ -51,20 +119,44 @@
 			<br>
 			</div>
 
-			<?php for($i=1;$i<=4;$i++){  ?>
-			<div class=" col-md-3">
+			
+			 <?php 
+
+                                            include('inc/db_connect.php'); 
+
+                                            $sql = "SELECT * FROM tbl_product LIMIT 20";
+                                            $result = $conn->query($sql);
+
+                                            if ($result->num_rows > 0) {
+                                              // output data of each row
+                                              while($row = $result->fetch_assoc()) {  ?> 
+
+                                      
+
+                                                <div class=" col-md-3">
 				<div class="card col-md-12 products">
 				
-				  <img src="img/4.jpg"  style="padding: 10px;" class="card-img-top" alt="Mobile Image">
+				  <img src="<?=$row['p_image']?>"  style="padding: 10px;" class="card-img-top" alt="<?=$row['p_name']?>">
 				  <div class="card-body">
-				    <h5 class="card-title">Samsung Mobile</h5>
-				    <p><b>Price $290 CAD</b></p>
-				    <p class="card-text">Samsung s21 latest mobile with</p>
-				    <a href="detail.php" class="btn btn-primary">View Detail</a>
+				    <h5 class="card-title"><?=$row['p_name']?></h5>
+				    <p><b>Price <?=$row['p_price']?> CAD</b></p>
+				    <p class="card-text"><?=substr($row['p_description'],0,50)?>...</p>
+				    <a href="detail.php?id=<?=$row['p_id']?>" class="btn btn-primary">View Detail</a>
 				  </div>
 				  </div>
 			</div>
-			<?php } ?>
+
+                                                
+                                             <?php 
+                                                 }
+                                                } else {
+                                                 // echo "0 results";
+                                                }
+                                            $conn->close();
+
+
+                                        ?>
+			
 
 
 		</div>
--- index.php
+++ index.php
@@ -9,22 +9,45 @@
 		<div class="container">
 			<h2 class="text-center"><br> Latest Products</h2>
 		<div class="row col-md-12">
-			
-			<?php for($i=1;$i<=12;$i++){  ?>
-			<div class=" col-md-3">
+
+			 <?php 
+
+                                            include('inc/db_connect.php'); 
+
+                                            $sql = "SELECT * FROM tbl_product LIMIT 20";
+                                            $result = $conn->query($sql);
+
+                                            if ($result->num_rows > 0) {
+                                              // output data of each row
+                                              while($row = $result->fetch_assoc()) {  ?> 
+
+                                      
+
+                                                <div class=" col-md-3">
 				<div class="card col-md-12 products">
 				
-				  <img src="img/4.jpg"  style="padding: 10px;" class="card-img-top" alt="Mobile Image">
+				  <img src="<?=$row['p_image']?>"  style="padding: 10px;" class="card-img-top" alt="<?=$row['p_name']?>">
 				  <div class="card-body">
-				    <h5 class="card-title">Samsung Mobile</h5>
-				    <p><b>Price $290 CAD</b></p>
-				    <p class="card-text">Samsung s21 latest mobile with</p>
-				    <a href="detail.php" class="btn btn-primary">View Detail</a>
+				    <h5 class="card-title"><?=$row['p_name']?></h5>
+				    <p><b>Price <?=$row['p_price']?> CAD</b></p>
+				    <p class="card-text"><?=substr($row['p_description'],0,50)?>...</p>
+				    <a href="detail.php?id=<?=$row['p_id']?>" class="btn btn-primary">View Detail</a>
 				  </div>
 				  </div>
 			</div>
-			<?php } ?>
 
+                                                
+                                             <?php 
+                                                 }
+                                                } else {
+                                                 // echo "0 results";
+                                                }
+                                            $conn->close();
+
+
+                                        ?>
+			
+		 
 
 		</div>
 
